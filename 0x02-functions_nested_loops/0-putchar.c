fc3d5b250145ce2a3fd5b1f397c67389f1d14dd6
KillMonga130 <mubvafhimoses813@gmail.com>
2022-09-13 10:05:12 -0700

    nested lops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * write a program that outputs "_putchar", followed by a nea line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
