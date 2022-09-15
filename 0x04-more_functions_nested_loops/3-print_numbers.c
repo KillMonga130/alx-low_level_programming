6be163e0bdc02d2db8f62e18c7c3a263f6e11f0c
KillMonga130 <mubvafhimoses813@gmail.com>
2022-09-15 12:42:40 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * Return: numbers
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
