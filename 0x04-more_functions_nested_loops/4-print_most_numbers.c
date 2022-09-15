95251a229e65dfc99cbc013c27775a376337cdec
KillMonga130 <mubvafhimoses813@gmail.com>
2022-09-15 12:46:53 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ * Return: numbers
+ */
+
+void print_most_numbers(void)
+{
+	char i = 0;
+
+	while (i <= 9)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
