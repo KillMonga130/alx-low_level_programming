e0816a3daf73d90c165df19930bae531e40b7b31
KillMonga130 <mubvafhimoses813@gmail.com>
2022-09-15 16:41:07 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_digonal - prints digonal line based on the value passed
+ * @n: the length of the line
+ * Return: line
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0, c;
+
+	while (i < n && n > 0)
+	{
+		c = 0;
+		while (c < i)
+		{
+			_putchar(' ');
+			c++;
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+}
