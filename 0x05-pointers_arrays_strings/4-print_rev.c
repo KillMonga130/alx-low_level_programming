25f1bcd613e2bcd13318e887c28d39199eeea2b7
KillMonga130 <mubvafhimoses813@gmail.com>
2022-09-19 09:02:12 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to be reversed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int reverse = 0;
+
+	while (s[reverse])
+		reverse++;
+
+	while (recverse--)
+	{
+		_putchar(s[reverse]);
+	}
+
+	_putchar('\n');
+}
