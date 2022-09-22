573ae5997afba4830e45065959f6545e1d08f809
KillMonga130 <mubvafhimoses813@gmail.com>
2022-09-22 14:27:52 -0700

    arrays

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - function like strcat, but will use at most n bytes
+ * @dest:  pointer
+ * @src: pointer
+ * @n: integer
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int m = 0, i;
+
+	for (i = 0; i < 1000; i++)
+	{
+		if (dest[i] == '\0')
+		{
+			break;
+		}
+		m++;
+	}
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[m + i] = src[i];
+	}
+	dest[m + i] = '\0';
+	return (dest);
+}
