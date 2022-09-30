3afbe2cb15c5f93721f80a5f770f23b281d38de5
KillMonga130 <mubvafhimoses813@gmail.com>
2022-09-30 08:44:12 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0, m;
+
+	while (i < argc)
+	{
+		m = 1;
+		i++;
+	}
+	printf("%d\n", m);
+
+	return (0);
+}
