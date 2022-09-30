572567ab9b0054e5c04bab1d9664a5d58f7b4cb2
KillMonga130 <mubvafhimoses813@gmail.com>
2022-09-30 09:00:55 -0700

    args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints all arguments it receives
@@ -13,7 +14,7 @@ int main(int argc, char *argv)
 
 	for (a = 0; a < argc; a++)
 	{
-		printf("%d\n", argv[a]);
+		printf("%s\n", argv[a]);
 	}
 
 	return (0);
